8eca6f90e989f983cb5d286b126e7de86d64db7b
Monica Reddy Baddam <monicareddyb6@gmail.com>
2024-02-29 12:34:44 -0600

    my first commit

---
+++ src/Loops/DoWhile.java
@@ -0,0 +1,14 @@
+package Loops;
+
+public class DoWhile {
+    public static class Main {
+        public static void main(String[] args) {
+            int i = 1;
+            do {
+                System.out.println("Hi" + i);
+                i++;
+            }
+            while (i <= 4);
+        }
+    }
+}
