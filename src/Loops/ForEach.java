d4ee637ce466dd7a078f2b7162cf8c3cccbeb8b9
Monica Reddy Baddam <monicareddyb6@gmail.com>
2024-02-29 12:51:58 -0600

    added foreach

---
+++ src/Loops/ForEach.java
@@ -0,0 +1,13 @@
+package Loops;
+
+public class ForEach {
+        public static void main(String[] args) {
+            // Define an array of integers
+            int[] numbers = {1, 2, 3, 4, 5};
+
+            // Iterate over the elements of the array using foreach loop
+            for (int number : numbers) {
+                System.out.println(number);
+            }
+        }
+    }
